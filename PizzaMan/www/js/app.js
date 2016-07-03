899146ec9f59d4276c7961e65a357e689e4b0cb3
mfossel <mikefossel@gmail.com>
2016-07-02 21:40:03 -0700

    initial setup

--- PizzaMan/www/js/app.js
+++ PizzaMan/www/js/app.js
@@ -22,3 +22,16 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'templates/home.html',
+      controller: 'HomeCtrl'
+    });
+
+  $urlRouterProvider.otherwise("/home");
+
+})
